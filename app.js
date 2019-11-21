f5bc96fdfa196c14c41bb684e72174fc0646b528
Chi Luu <820742@ACDM.DS.SAIT.CA>
2019-11-21 16:59:52 -0700

    **Converted to Express Project**
    Changed to Express project.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+let server = app.listen(8001, ()=> {
+      var host = server.address().address;
+      var port = server.address().port;
+      
+      console.log("Server is listening on port 8000. Ready to accept requests\n" + 
+                  "Host Address: " + host + "\nPort: " + port);
+});
+
+app.use(express.static('views'));
+app.use(express.static('public'));
+
+
+app.use((req, res, next)=>{
+   res.status(404).sendFile(__dirname + "/views/404.html");
+});
