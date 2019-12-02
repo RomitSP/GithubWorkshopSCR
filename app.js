482a1451b7558d599cf39b24d56828818fbcd33f
Chi Luu <35760570+geez2k@users.noreply.github.com>
2019-12-01 20:33:39 -0700

    Completed Registration Form's JS code
    Added email check.
    Updated postal code check with strict conditions.
    Fixed "PROTOCOL_ENQUEUE_AFTER_QUIT" error on "sql.createConnection()" issue: https://github.com/mysqljs/mysql/issues/1213
    Used connection createPool() and connection.release() instead of createConnection and connection.end().
    Added background.

--- app.js
+++ app.js
@@ -1,18 +1,57 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
-let server = app.listen(8001, ()=> {
-      var host = server.address().address;
-      var port = server.address().port;
-      
-      console.log("Server is listening on port 8000. Ready to accept requests\n" + 
-                  "Host Address: " + host + "\nPort: " + port);
+let server = app.listen(8001, () => {
+   var host = server.address().address;
+   var port = server.address().port;
+
+   console.log("Server is listening on port 8000. Ready to accept requests\n" +
+      "Host Address: " + host + "\nPort: " + port);
 });
 
-app.use(express.static('views', {extensions:['html', 'htm'], index: 'home.html'}));
+app.use(express.static('views', { extensions: ['html', 'htm'], index: 'home.html' }));
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let sqlCon = mysql.createPool({
+   connectionLimit: 10,
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   database: 'travelexperts'
+});
+
+app.post('/register', (req, res) => {
+   console.log(req.body);
+   
+   let data = [req.body.fname, req.body.lname, req.body.addr,
+   req.body.city, req.body.prov, req.body.pCode,
+   req.body.country, req.body.hPhone, req.body.bPhone,
+   req.body.email, 1];
+
+   sqlCon.getConnection((err, connection) => {
+      if (err) throw err;
+      console.log('Connected!');
+
+      let sql = "INSERT INTO `customers`(`CustFirstName`, `CustLastName`," +
+         " `CustAddress`, `CustCity`, `CustProv`, `CustPostal`, `CustCountry`, " +
+         "`CustHomePhone`, `CustBusPhone`, `CustEmail`, `AgentId`) VALUES" +
+         "(?,?,?,?,?,?,?,?,?,?,?);";
+
+      sqlCon.query(sql, data, (err, result, fields) => {
+         if (err) throw err;
+         console.log("Result: " + JSON.stringify(result, undefined, 2));
+
+         connection.release();
+      });
+   });
+
+   res.redirect('thanks');
+});
 
 
 app.use((req, res, next)=>{
-   res.status(404).sendFile(__dirname + "/views/404.html");
+   res.status(404).sendFile(__dirname + "/views/404.html"); 
 });
--- public/register-script.js
+++ public/register-script.js
@@ -1,47 +1,64 @@
 function confirmchoice() {
-   var myform = document.forms[0];
+  var myform = document.forms[0];
 
-   if (myform.fname.value == "") {
-     myform.fname.focus();
-     alert("First name can not be blank!");
-     return false;
-   }
-   else if(myform.lname.value == "") {
-     myform.lname.focus();
-     alert("Last name can not be blank!");
-     return false;
-   }
-   else if(!checkPC(myform.pCode.value)){
-       myform.pCode.focus();
-       alert("Invalid Postal Code.");
-       return checkPC(myform.pCode.value);
-   }
+  if (myform.fname.value == "") {
+    myform.fname.focus();
+    alert("First name can not be blank!");
+    return false;
+  }
+  else if (myform.lname.value == "") {
+    myform.lname.focus();
+    alert("Last name can not be blank!");
+    return false;
+  }
+  // Postal Code Validation
+  else if (!checkPC(myform.pCode.value)) {
+    myform.pCode.focus();
+    alert("Invalid Postal Code");
+    return checkPC(myform.pCode.value);
+  }
+  // Email Validation
+  else if (!checkEmail(myform.email.value)) {
+    alert("Invalid Email address");
+    return checkEmail(myform.email.value);
+  }
+  else return true;
+}
 
-   return confirm("Are you sure you want to send?");
- }
+// Strick Canadian Postal Code Validation
+// https://stackoverflow.com/questions/15774555/efficient-regex-for-canadian-postal-code-function
+function checkPC(pCode) {
+  var myReg = /^[ABCEGHJ-NPRSTVXY][0-9][ABCEGHJ-NPRSTV-Z]\s?[0-9][ABCEGHJ-NPRSTV-Z][0-9]$/i;
 
- function checkPC(pCode) {
-     var myReg = /^[a-z|A-Z][0-9][a-z|A-Z]\s?[0-9][a-z|A-Z][0-9]$/;
+  return myReg.test(pCode);
+}
 
-     return myReg.test(pCode);
- }
+// Email validation
+// https://www.regular-expressions.info/email.html
+function checkEmail(email) {
+  var myReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
- var message = "";
- var formInfo = new Array();
+  return myReg.test(email);
+}
 
- formInfo['fname'] = "Enter your first name here";
- formInfo['lname'] = "Enter your last name here";
- formInfo['addr'] = "Mailing address here";
- formInfo['city'] = "Your city name here";
- formInfo['pCode'] = "Postal Code (<i>E.g.</i>T1P 3H2)";
- formInfo['prov'] = "Select 2 digit province code";
- formInfo['email'] = "Valid email here";
+var helpInfo = {
+  'fname': "Enter your first name here",
+  'lname': "Enter your last name here",
+  'addr': "Mailing address",
+  'city': "Your city name",
+  'prov': "Select 2 digit province code",
+  'pCode': "Postal Code (<i>E.g.</i>T1P 3H2)",
+  'country': "Your country",
+  'hPhone': "Your Home Phone #",
+  'bPhone': "Your Business Phone #",
+  'email': "Valid email here"
+};
 
- function focusText(element) {
-     element.nextElementSibling.style.visibility = 'visible';
-     element.nextElementSibling.innerHTML = formInfo[element.name];
- }
- function blurText(element) {
-     element.nextElementSibling.style.visibility = 'hidden';
-     element.nextElementSibling.innerHTML = "";
- }
+function focusText(element) {
+  element.nextElementSibling.style.visibility = 'visible';
+  element.nextElementSibling.innerHTML = helpInfo[element.name];
+}
+function blurText(element) {
+  element.nextElementSibling.style.visibility = 'hidden';
+  element.nextElementSibling.innerHTML = "";
+}
