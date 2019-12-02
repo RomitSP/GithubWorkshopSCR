d648ec2986ced48450608122a53fadefe81af209
Chi Luu <35760570+geez2k@users.noreply.github.com>
2019-12-02 00:23:50 -0700

    Added EJS, Contact Page, and Vacation Packages page
    Created Contact Page and Vacation Packages Page with EJS.
    Allowed drawing data from database.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const ejs = require('ejs');
 
 let server = app.listen(8001, () => {
    var host = server.address().address;
@@ -11,8 +12,9 @@ let server = app.listen(8001, () => {
       "Host Address: " + host + "\nPort: " + port);
 });
 
-app.use(express.static('views', { extensions: ['html', 'htm'], index: 'home.html' }));
+app.use(express.static('views', { extensions: [ 'html', 'htm' ], index: 'home.html'}));
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let sqlCon = mysql.createPool({
@@ -23,27 +25,61 @@ let sqlCon = mysql.createPool({
    database: 'travelexperts'
 });
 
-app.post('/register', (req, res) => {
-   console.log(req.body);
-   
-   let data = [req.body.fname, req.body.lname, req.body.addr,
-   req.body.city, req.body.prov, req.body.pCode,
-   req.body.country, req.body.hPhone, req.body.bPhone,
-   req.body.email, 1];
+// Render with EJS
+// Images are place holder to render with each of the 4 vacation packages
+let images = ['Amsterdam.jpg', 'HotelView.jpg', 'Rialto.jpg', 'rome.jfif', 'Faraglioni.jpg'];
+app.get('/vPackages', (req, res) => {
+   let sql = "SELECT `PkgName`, `PkgStartDate`, `PkgEndDate`, `PkgDesc`, " +
+             "`PkgBasePrice` FROM `packages` ";
 
    sqlCon.getConnection((err, connection) => {
       if (err) throw err;
       console.log('Connected!');
 
-      let sql = "INSERT INTO `customers`(`CustFirstName`, `CustLastName`," +
+      sqlCon.query(sql, (err, vPackages) => {
+         if(err) throw err;
+         let index = 0;
+         res.render('vPackages', { vPackages, images, index });
+         connection.release();
+      });
+   });
+});
+
+// Render with EJS
+app.get('/contact', (req, res) => {
+
+   let sql = "SELECT `AgtFirstName`, `AgtLastName`, `AgtBusPhone`, `AgtEmail`," + 
+             " `AgtPosition`, `AgencyId` FROM `agents`";
+
+   sqlCon.getConnection((err, connection) => {
+      if (err) throw err;
+      console.log('Connected!');
+
+      sqlCon.query(sql, (err, agents) => {
+         if(err) throw err;
+         res.render('contact', { agents });
+         connection.release();
+      });
+   });
+});
+
+app.post('/register', (req, res) => {   
+   let data = [req.body.fname, req.body.lname, req.body.addr,
+   req.body.city, req.body.prov, req.body.pCode,
+   req.body.country, req.body.hPhone, req.body.bPhone,
+   req.body.email, 1];
+
+   let sql = "INSERT INTO `customers`(`CustFirstName`, `CustLastName`," +
          " `CustAddress`, `CustCity`, `CustProv`, `CustPostal`, `CustCountry`, " +
          "`CustHomePhone`, `CustBusPhone`, `CustEmail`, `AgentId`) VALUES" +
          "(?,?,?,?,?,?,?,?,?,?,?);";
 
+   sqlCon.getConnection((err, connection) => {
+      if (err) throw err;
+      console.log('Connected!');
+
       sqlCon.query(sql, data, (err, result, fields) => {
          if (err) throw err;
-         console.log("Result: " + JSON.stringify(result, undefined, 2));
-
          connection.release();
       });
    });
