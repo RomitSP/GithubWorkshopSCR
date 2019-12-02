cc62a22b95b6b13c8566bfcf15cbe886732575a3
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2019-12-02 15:57:43 -0700

    monday afternoon

---
+++ public/jsfunctions.js
@@ -0,0 +1,21 @@
+	
+	//Preview videos for vacation package
+	function caribbeanVideo() 
+		{
+			window.open("https://www.youtube.com/watch?v=trmEq3e04a4")
+		}
+		
+		function polyVideo() 
+		{
+				window.open("https://www.youtube.com/watch?v=ZZ9E-Jadv88")
+		}
+		
+		function asiaVideo() 
+		{
+			window.open("https://www.youtube.com/watch?v=vBMBixI40Nw")
+		}
+		
+		function europeVideo() 
+		{
+			window.open("https://www.youtube.com/watch?v=UfEiKK-iX70")
+		}
