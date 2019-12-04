da0fd91c8c1236655946cd76475b06175926c3dd
Chi Luu <35760570+geez2k@users.noreply.github.com>
2019-12-03 20:55:04 -0700

    Merged all member's code
    1)Converted contact and vpackage to .ejs.
    2)Contact, Vacation Package, and Register pages tested and working with DB.
    3) Cleaned up code and files.

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const mysql = require('mysql');
 const ejs = require('ejs');
 
 let server = app.listen(8001, () => {
-   var host = server.address().address;
-   var port = server.address().port;
+   const host = server.address().address;
+   const port = server.address().port;
 
    console.log("Server is listening on port 8000. Ready to accept requests\n" +
       "Host Address: " + host + "\nPort: " + port);
 });
 
-app.use(express.static('views', { extensions: [ 'html', 'htm' ], index: 'home.html'}));
+app.use(express.static('views', { extensions: ['html', 'htm'], index: 'home.html' }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,18 +28,18 @@ let sqlCon = mysql.createPool({
 // Render with EJS
 // Images are place holder to render with each of the 4 vacation packages
 let images = ['Amsterdam.jpg', 'HotelView.jpg', 'Rialto.jpg', 'rome.jfif', 'Faraglioni.jpg'];
-app.get('/vPackages', (req, res) => {
+app.get('/vPackagesForm', (req, res) => {
    let sql = "SELECT `PkgName`, `PkgStartDate`, `PkgEndDate`, `PkgDesc`, " +
-             "`PkgBasePrice` FROM `packages` ";
+      "`PkgBasePrice` FROM `packages` ";
 
    sqlCon.getConnection((err, connection) => {
       if (err) throw err;
       console.log('Connected!');
 
-      sqlCon.query(sql, (err, vPackages) => {
-         if(err) throw err;
+      sqlCon.query(sql, (err, packages) => {
+         if (err) throw err;
          let index = 0;
-         res.render('vPackages', { vPackages, images, index });
+         res.render('vPackagesForm', { packages, images, index });
          connection.release();
       });
    });
@@ -48,15 +48,15 @@ app.get('/vPackages', (req, res) => {
 // Render with EJS
 app.get('/chi_contact', (req, res) => {
 
-   let sql = "SELECT `AgtFirstName`, `AgtLastName`, `AgtBusPhone`, `AgtEmail`," + 
-             " `AgtPosition`, `AgencyId` FROM `agents`";
+   let sql = "SELECT `AgtFirstName`, `AgtLastName`, `AgtBusPhone`, `AgtEmail`," +
+      " `AgtPosition`, `AgencyId` FROM `agents`";
 
    sqlCon.getConnection((err, connection) => {
       if (err) throw err;
       console.log('Connected!');
 
       sqlCon.query(sql, (err, agents) => {
-         if(err) throw err;
+         if (err) throw err;
          res.render('chi_contact', { agents });
          connection.release();
       });
@@ -66,32 +66,31 @@ app.get('/chi_contact', (req, res) => {
 // Render with EJS
 app.get('/contact', (req, res) => {
 
-   let sql = "SELECT `AgtFirstName`, `AgtLastName`, `AgtBusPhone`, `AgtEmail`," + 
-             " `AgtPosition`, `AgencyId` FROM `agents`";
+   let sql = "SELECT `AgtFirstName`, `AgtLastName`, `AgtBusPhone`, `AgtEmail`," +
+      " `AgtPosition`, `AgencyId` FROM `agents`";
 
    sqlCon.getConnection((err, connection) => {
       if (err) throw err;
       console.log('Connected!');
 
       sqlCon.query(sql, (err, agents) => {
-         if(err) throw err;
-         console.log(agents);
+         if (err) throw err;
          res.render('contact', { agents });
          connection.release();
       });
    });
 });
 
-app.post('/register', (req, res) => {   
+app.post('/register', (req, res) => {
    let data = [req.body.fname, req.body.lname, req.body.addr,
    req.body.city, req.body.prov, req.body.pCode,
    req.body.country, req.body.hPhone, req.body.bPhone,
    req.body.email, 1];
 
    let sql = "INSERT INTO `customers`(`CustFirstName`, `CustLastName`," +
-         " `CustAddress`, `CustCity`, `CustProv`, `CustPostal`, `CustCountry`, " +
-         "`CustHomePhone`, `CustBusPhone`, `CustEmail`, `AgentId`) VALUES" +
-         "(?,?,?,?,?,?,?,?,?,?,?);";
+      " `CustAddress`, `CustCity`, `CustProv`, `CustPostal`, `CustCountry`, " +
+      "`CustHomePhone`, `CustBusPhone`, `CustEmail`, `AgentId`) VALUES" +
+      "(?,?,?,?,?,?,?,?,?,?,?);";
 
    sqlCon.getConnection((err, connection) => {
       if (err) throw err;
@@ -106,7 +105,25 @@ app.post('/register', (req, res) => {
    res.redirect('thanks');
 });
 
+app.post("/vPackages_form", (req, res) => {
+   let data = [req.body.dateLeaving, req.body.dateReturning, req.body.leavingFrom, req.body.vacaPackage];
 
-app.use((req, res, next)=>{
-   res.status(404).sendFile(__dirname + "/views/404.html"); 
+   sqlCon.getConnection((err, connection) => {
+      if (err) throw err;
+
+      let sql = "INSERT INTO `orders`(`dateLeaving`, `dateReturning`,"
+         + " `leavingFrom`, `packageChosen`) "
+         + "VALUES (?,?,?,?)";
+      sqlCon.query(sql, data, (err, result, fields) => {
+         if (err) throw err;
+         connection.release();
+      });
+   });
+
+   res.redirect('thanks');
+});
+
+
+app.use((req, res, next) => {
+   res.status(404).sendFile(__dirname + "/views/404.html");
 });
